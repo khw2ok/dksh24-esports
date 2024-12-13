7ee9dc3d572a73bc432e804e19d856152715eefc
김형욱 <idkimwook@gmail.com>
2024-12-14 01:35:07 +0900

    Initial commit, saving progress

---
+++ src/routes/+page.svelte
@@ -0,0 +1,320 @@
+<!--
+<header class="p-2 border-b">
+  <div class="flex justify-between p-1 mx-auto" style="max-width: 800px;">
+    <a href="?">DKSH24 eSports Tournaments Official</a>
+    <span>By @khw2ok (10308 김형욱)</span>
+  </div>
+</header>
+
+<div class="p-2 border-b">
+  <div class="flex justify-between mx-auto" style="max-width: 800px;">
+    <button>홈</button>
+    <button class="">계정</button>
+  </div>
+</div>
+
+<div class="absolute w-full h-full -z-10 bg-neutral-50"></div> -->
+
+<div class="mb-4 sm:mb-6"></div>
+
+<div class="p-4 mx-auto text-sm font-light bg-white sm:text-base" style="max-width: 800px;">
+  <span class="text-gray-500">Managed by <a class="hover:text-red-500 hover:underline" href="https://github.com/khw2ok">@khw2ok</a> (10308 김형욱)</span>
+  <h1 class="text-3xl font-bold sm:text-4xl ">DKSH24 eSports Tournaments<br>Official Information Site</h1>
+  <span class="text-gray-500 sm:text-lg">Last modified on December 13, 2024</span>
+  <hr class="my-2 border-b">
+  <span class="text-xl">Until next game</span>
+  <h2 class="text-4xl font-medium text-center text-red-600">12h 10m 10s</h2>
+  <hr class="my-2 border-b">
+  <div class="mb-4"></div>
+  <div id="bracket-lol" class="relative flex justify-center border rounded-lg bg-gray-50">
+    <div class="flex px-4 py-2 overflow-auto scroll-auto">
+      <!-- one start -->
+      <div class="column">
+        <div class="match">
+          <div class="!bg-red-200 team">
+            <span class="name">1-3</span>
+            <span class="score">0</span>
+          </div>
+          <div class="!bg-red-200 team">
+            <span class="name">1-4</span>
+            <span class="score">0</span>
+          </div>
+          <div class="match-lines">
+            <div class="line1"></div>
+            <div class="line2-down"></div>
+          </div>
+          <div class="match-lines alt">
+            <div class="line1"></div>
+          </div>
+        </div>
+        <div class="match">
+          <div class="team">
+            <span class="name">1-1</span>
+            <span class="score">0</span>
+          </div>
+          <div class="team">
+            <span class="name">1-2</span>
+            <span class="score">0</span>
+          </div>
+          <div class="match-lines">
+            <div class="line1"></div>
+            <div class="line2-up"></div>
+          </div>
+          <div class="match-lines alt">
+            <div class="line1"></div>
+          </div>
+        </div>
+        <div class="match">
+          <div class="team">
+            <span class="name">2-1</span>
+            <span class="score">0</span>
+          </div>
+          <div class="team">
+            <span class="name">2-2</span>
+            <span class="score">0</span>
+          </div>
+          <div class="match-lines">
+            <div class="line1"></div>
+            <!-- <div class="line2-up"></div> -->
+          </div>
+          <div class="match-lines alt">
+            <div class="line1"></div>
+          </div>
+        </div>
+        <div class="match">
+          <div class="team">
+            <span class="name">1-5</span>
+            <span class="score">0</span>
+          </div>
+          <div class="team">
+            <span class="name">2-3</span>
+            <span class="score">0</span>
+          </div>
+          <div class="match-lines">
+            <div class="line1"></div>
+            <div class="line2-down"></div>
+          </div>
+          <div class="match-lines alt">
+            <div class="line1"></div>
+          </div>
+        </div>
+        <div class="match !h-8">
+          <div class="team">
+            <span class="name">2-4</span>
+            <span class="score"></span>
+          </div>
+          <div class="match-lines">
+            <div class="line1"></div>
+            <div class="line2-up"></div>
+          </div>
+          <div class="match-lines alt">
+            <div class="line1"></div>
+          </div>
+        </div>
+      </div>
+      <!-- two start -->
+      <div class="column" style="padding-top: 35px; padding-bottom: 3px;">
+        <div class="match">
+          <div class="!bg-red-100 team">
+            <span class="name"></span>
+            <span class="score"></span>
+          </div>
+          <div class="team">
+            <span class="name"></span>
+            <span class="score"></span>
+          </div>
+          <div class="match-lines">
+            <div class="line1"></div>
+            <!-- <div class="line2-up"></div> -->
+          </div>
+          <div class="match-lines alt">
+            <div class="line1"></div>
+          </div>
+        </div>
+        <div class="match !h-8">
+          <!-- <div class="team">
+            <span class="name"></span>
+            <span class="score"></span>
+          </div> -->
+          <div class="absolute w-24 top-1/2" style="background: lightgray; height: 1px;"></div>
+          <div class="match-lines">
+            <div class="line1"></div>
+            <div class="line2-down"></div>
+          </div>
+          <div class="match-lines alt">
+            <div class="line1"></div>
+          </div>
+        </div>
+        <div class="match">
+          <div class="team">
+            <span class="name"></span>
+            <span class="score"></span>
+          </div>
+          <div class="team">
+            <span class="name">2-4</span>
+            <span class="score">0</span>
+          </div>
+          <div class="match-lines">
+            <div class="line1"></div>
+            <div class="line2-up"></div>
+          </div>
+          <div class="match-lines alt">
+            <div class="line1"></div>
+          </div>
+        </div>
+      </div>
+      <!-- three start -->
+      <div class="column">
+        <div class="match">
+          <div class="absolute w-24 top-1/2" style="background: lightgray; height: 1px;"></div>
+          <div class="match-lines">
+            <div class="line1"></div>
+            <div class="line2-down"></div>
+          </div>
+          <div class="match-lines alt">
+            <div class="line1"></div>
+          </div>
+        </div>
+        <div class="match">
+          <div class="team">
+            <span class="name"></span>
+            <span class="score"></span>
+          </div>
+          <div class="team">
+            <span class="name"></span>
+            <span class="score"></span>
+          </div>
+          <div class="match-lines">
+            <div class="line1"></div>
+            <div class="line2-up"></div>
+          </div>
+          <div class="match-lines alt">
+            <div class="line1"></div>
+          </div>
+        </div>
+      </div>
+      <!-- four start -->
+      <div class="column">
+      <div class="match">
+        <div class="team">
+          <span class="name"></span>
+          <span class="score"></span>
+        </div>
+        <div class="team">
+          <span class="name"></span>
+          <span class="score"></span>
+        </div>
+        <div class="match-lines">
+          <div class="line1"></div>
+          <!-- <div class="line2-down"></div> -->
+        </div>
+        <div class="match-lines alt">
+          <div class="line1"></div>
+        </div>
+      </div>
+      </div>
+      <!-- five start -->
+      <div class="column">
+        <div class="match !h-8">
+          <div class="team">
+            <span class="name"></span>
+            <span class="score"></span>
+          </div>
+          <div class="match-lines">
+            <div class="line1"></div>
+            <div class="line2-down"></div>
+          </div>
+          <div class="match-lines alt">
+            <div class="line1"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- todo: input toggle make -->
+    <input type="checkbox" id="bracket-toggle" class="absolute left-0 flex m-2">
+  </div>
+</div>
+
+<!-- <footer class="absolute p-4 mx-auto bg-white border-t" style="max-width: 1000px;"></footer> -->
+
+
+<style type="tailwind/components">
+::selection {
+  @apply bg-red-600 bg-opacity-20;
+}
+
+.column {
+  @apply flex flex-col min-h-full justify-around;
+}
+.match {
+  @apply relative flex flex-col min-w-24 max-w-24 h-16 my-3 mr-6;
+}
+.match .team {
+  @apply flex items-center w-full h-full border border-black bg-white;
+}
+.match .team span {
+  @apply pl-2;
+}
+.match .team span:last-child {
+  @apply pr-2;
+}
+.match .team .score {
+  @apply ml-auto;
+}
+.match .team:first-child {
+  margin-bottom: -1px;
+}
+.match-lines {
+  position: absolute;
+  top: 50%;
+  bottom: 0;
+  margin-top: 0px;
+  right: -1px;
+}
+.line1 {
+  background: lightgray;
+  position: absolute;
+  height: 1px;
+  width: 12px;
+}
+.line2-down {
+  background: lightgray;
+  position: absolute;
+  height: 44px;
+  width: 1px;
+  left: 11px;
+}
+.line2-up {
+  background: lightgray;
+  position: absolute;
+  height: 44px;
+  width: 1px;
+  left: 11px;
+  transform: translate(0, -100%);
+}
+.match-lines.alt {
+  left: -12px;
+}
+
+.column:first-child .match-lines.alt {
+  display: none;
+}
+.column:last-child .match-lines {
+  display: none;
+}
+.column:last-child .match-lines.alt {
+  display: block;
+}
+.column:nth-child(2) .match-lines .line2-down {
+  height: 88px;
+}
+.column:nth-child(3) .match-lines .line2-down {
+  height: 175px;
+}
+.column:nth-child(4) .match-lines .line2-down {
+  height: 262px;
+}
+.column:nth-child(5) .match-lines .line2-down {
+  height: 349px;
+}
+</style>
